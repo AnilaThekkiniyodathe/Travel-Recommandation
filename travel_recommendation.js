d7e06049f4c952340a48b13119053de3338194e5
AnilaThekkiniyodathe <amanna093@gmail.com>
2024-05-15 18:16:26 -0400

    searchbar update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -6,27 +6,37 @@ function searchRecommandations(){
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-        const condition = data.beaches.find();
-        const keyword = data.beaches.find(item => item.name.toLowerCase() === input);
-
-        if (condition) {
-          const symptoms = condition.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
-
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-        } else {
-          resultDiv.innerHTML = 'Condition not found.';
+        if(input=="countries"){
+            const countries = data.countries;
+            recommandationResult(countries);
         }
-      })
+        if(input=="temples"){
+            const temples = data.temples;
+            recommandationResult(temples);
+        }
+        if(input=="beaches"){
+            const beaches = data.beaches;
+            recommandationResult(beaches);
+        }
+       
+        function recommandationResult(keywords){
+        keywords.forEach(function(keyword) {
+          const name = keyword.name;
+          const imageUrl = keyword.imageUrl;
+          const description = keyword.description;
+          
+          resultDiv.innerHTML += `<img src="${imageUrl}" alt="hjh">`;
+          resultDiv.innerHTML += `<h4 style="font-weight:500">${name}</h4>`;          
+          resultDiv.innerHTML += `<p>${description}</p>`;
+          
+          })
+        }
+        } )
+      
       .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
       });
   }
+
 
